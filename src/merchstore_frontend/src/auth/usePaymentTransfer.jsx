80012afee916c74605396edab14d10c056ab1c0e
ankurnayak1 <ankur.nayak@quadbtech.com>
2024-08-12 17:01:32 +0530

    finalize_order_6

--- src/merchstore_frontend/src/auth/usePaymentTransfer.jsx
+++ src/merchstore_frontend/src/auth/usePaymentTransfer.jsx
@@ -7,11 +7,10 @@ const usePaymentTransfer = () => {
   const transfer = async (to, amount, memo = "") => {
     setLoading(true);
     setError(null);
-    const roundOfAmt = Math.round(amount);
     try {
       const transferWindow = await window.ic.plug.requestTransfer({
         to,
-        roundOfAmt,
+        amount,
         memo,
       });
       return transferWindow;
